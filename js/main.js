dbc5de22d8b2f97fdf3d02bf17f0724b1f5ca54e
faanna <sakurayu96@gmil.com>
2023-11-27 16:19:02 +0900

    scroll

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const sections = document.querySelectorAll('section');
+let postionArray = [];
+
+window.addEventListener('scroll', () => {
+	// 스크롤은 무조건 윈도우에만 할수있음
+	let scroll = window.scrollY;
+	// 스크롤에도 칸수가 적용되있음
+	// ->기본픽셀 100px : 한번움직이고 뒤로갈때마다 100px 증가/ 감소
+});
